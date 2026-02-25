265ae6af96832741f025d534c6c199e4e3cb455b
Mayuresh Patel <mayureshpatel1795@gmail.com>
2026-02-24 20:27:04 -0500

    Remove core index barrel file and associated exports

--- src/app/core/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './auth/auth.service';
-export * from './auth/auth.interceptor';
-export * from './auth/auth.guard';
-export * from './services/storage.service';
-export * from './services/toast.service';
