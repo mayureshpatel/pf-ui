2dd141375bf2fba949070975aed1110944fbfa9f
Mayuresh Patel <mayureshpatel1795@gmail.com>
2026-03-06 10:51:07 -0500

    Refactor `formatDate` to `convertDateString` with enhanced flexibility:
    - Add optional parameters for custom format options and locale.
    - Update JSDoc comments for improved code clarity and usage.

--- src/app/shared/utils/transaction.utils.ts
+++ src/app/shared/utils/transaction.utils.ts
@@ -1,7 +1,17 @@
-export function formatDate(dateString: string): string {
-  return new Date(dateString).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric'
-  });
+/**
+ * Converts a date string to a localized format.
+ * The default format is "MM/DD/YYYY" with English locale.
+ * @param dateString The date string to format.
+ * @param formatOptions
+ * @param locale The locale to use for formatting the date.
+ */
+export function convertDateString(
+  dateString: string,
+  formatOptions?: Intl.DateTimeFormatOptions,
+  locale?: Intl.LocalesArgument
+): string {
+  formatOptions ??= {year: 'numeric', month: 'short', day: 'numeric'};
+  locale ??= 'en-US';
+
+  return new Date(dateString).toLocaleDateString(locale, formatOptions);
 }
